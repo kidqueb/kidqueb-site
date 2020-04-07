cccbc2756fe37fd8b5d51769022a9946826dea09
Nick Quebbeman <n.quebb@gmail.com>
2020-04-06 19:19:01 -0700

    not doing much

---
+++ content/collections/pages/articles.md
@@ -0,0 +1,8 @@
+---
+title: Articles
+author: cd4ea015-a792-46cb-a2b3-bdfdb086e840
+template: articles/index
+updated_by: cd4ea015-a792-46cb-a2b3-bdfdb086e840
+updated_at: 1586221138
+id: 31e6bf3c-4adb-419e-81ed-efdb7de5f78c
+---
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,38 @@
+// import "./node_modules/tailwindcss/stubs/defaultConfig.stub.js"
+
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: [
+          'IBM Plex Sans',
+          'system-ui',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Helvetica Neue"',
+          'sans-serif',
+        ],
+        mono: ['IBM Plex Mono', 'Menlo', 'Monaco', 'Consolas', '"Liberation Mono"', '"Courier New"', 'monospace'],
+      },
+      colors: {
+        black: '#171513',
+        white: '#E6E0D7',
+        gray: {
+          '900': '#2B2826'
+        }
+      },
+      maxWidth: {
+        'logomark': '11.25rem',
+        'wordmark': '16rem'
+      },
+      opacity: {
+        '10': '0.1',
+        '20': '0.2',
+        '30': '0.3',
+        '40': '0.4',
+      },
+      container: false
+    },
   },
   variants: {},
   plugins: [],
